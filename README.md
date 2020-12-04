34509a61d38c3079d538430873d7903b91a2f26e
James Barciz <jamesjbarciz@gmail.com>
2020-12-04 11:14:41 -0500

    Remove Basilica embedding method and replace with SpaCy vectorizer

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # twitoff-ds16-jjb
 A Flask web application for comparing Twitter users
+
+
+```
+flask
+jinja2
+flask-sqlalchemy
+tweepy
+basilica
+python-dotenv
+scikit-learn
+gunicorn
+psycopg2
+```
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -1,36 +1,29 @@
 #!/usr/bin/env Python
 """Retrieve Tweets, embeddings, and persist in the database."""
 import tweepy
-import basilica
-import os
-from dotenv import load_dotenv
+import spacy
+from decouple import config
 
 from .models import DB, Tweet, User
 
-load_dotenv()
-
-TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
-TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
-BASILICA_KEY = os.getenv("BASILICA_KEY")
 
 TWITTER_AUTH = tweepy.OAuthHandler(
-    TWITTER_API_KEY,
-    TWITTER_API_SECRET
+    config('TWITTER_API_KEY'),
+    config('TWITTER_API_SECRET')
 )
+
+TWITTER_AUTH.set_access_token(
+    config('TWITTER_ACCESS_TOKEN'),
+    config('TWITTER_ACCESS_TOKEN_SECRET')
+)
+
 TWITTER = tweepy.API(TWITTER_AUTH)
-
-BASILICA = basilica.Connection(BASILICA_KEY)
-
-
-TWITTER_USERS = ['calebhicks', 'elonmusk', 'rrherr', 'SteveMartinToGo',
-                 'alyankovic', 'nasa', 'sadserver', 'jkhowland', 'austen',
-                 'common_squirrel', 'KenJennings', 'conanobrien',
-                 'big_ben_clock', 'IAM_SHAKESPEARE']
 
 
 def add_or_update_user(username):
     """Add or update a user and their Tweets, error if not a Twitter user."""
     try:
+        nlp = spacy.load('en_core_web_lg')
         twitter_user = TWITTER.get_user(username)
         db_user = (User.query.get(twitter_user.id) or
                    User(id=twitter_user.id, name=username))
@@ -43,8 +36,7 @@ def add_or_update_user(username):
         if tweets:
             db_user.newest_tweet_id = tweets[0].id
         for tweet in tweets:
-            embedding = BASILICA.embed_sentence(tweet.full_text,
-                                                model='twitter')
+            embedding = list(nlp(tweet.text).vector)
             db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300],
                              embedding=embedding)
             db_user.tweets.append(db_tweet)
@@ -54,13 +46,6 @@ def add_or_update_user(username):
         raise e
     else:
         DB.session.commit()
-
-
-def add_users(users=TWITTER_USERS):
-    """Add/update a list of users."""
-    for user in users:
-        add_or_update_user(user)
-    # add_or_update_user(users[0])
 
 
 def update_all_users():
